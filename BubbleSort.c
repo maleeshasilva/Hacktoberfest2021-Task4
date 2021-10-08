f3a4adb02429a2d89905be9ac0dd69049825cd44
Maleesha Dinoli Silva <56461176+maleeshasilva@users.noreply.github.com>
2021-10-08 20:35:42 +0530

    Update BubbleSort.c

--- BubbleSort.c
+++ BubbleSort.c
@@ -12,17 +12,18 @@ int main()
         swap=false;
         for(j=0;j<n;j++)
         {
-            if (arr[j]>arr[j-1]);
+            if (arr[j]>arr[j+1])
             {
                 temp=arr[j];
                 arr[j]=arr[j+1];
                 arr[j+1]=temp
-                swap=false;
+                swap=true;
             }
         }
     }while(swap);
     
-    for(m=0;m<n;m--)
+    
+    for(j=0;j<n;j++)    
         {
             printf("%s ",arr[l]);
         }
